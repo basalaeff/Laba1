04932ecf179d50e7a14e553f158dab5735dedac0
basalaeff <163347696+basalaeff@users.noreply.github.com>
2024-09-09 02:47:43 +0700

    Delete circle

--- Geometric shapes.cpp
+++ Geometric shapes.cpp
@@ -3,34 +3,6 @@
 
 using namespace std;
 
-double calculateCircleLength(double pi, double radius) {
-    return 2 * pi * radius;
-}
-
-double calculateCircleArea(double pi, double radius) {
-    return pi * radius * radius;
-}
-
-double calculateCircularSector(double pi, double radius, double angle) {
-    return (pi * radius * radius * angle) / 360;
-}
-
-void circle() {
-    double pi = 3.14;
-    cout << "Введите радиус: ";
-    double radius = 0;
-    cin >> radius;
-    double CircleLength = calculateCircleLength(pi, radius);
-    cout << "Длина окружности: " << CircleLength << endl;
-    double CircleArea = calculateCircleArea(pi, radius);
-    cout << "Площадь круга: " << CircleArea << endl;
-    cout << "Введите угол для подсчета площади кругового сектора: ";
-    double angle;
-    cin >> angle;
-    double CircularSector = calculateCircularSector(pi, radius, angle);
-    cout << "Площадь кругового сектора: " << CircularSector << endl;
-}
-
 int main() {
     setlocale(LC_ALL, "Rus");
     while (true) {
@@ -45,7 +17,7 @@ int main() {
         switch (shape) {
         case 1:
             cout << "Окружность" << endl << endl;
-            circle();
+            cout << "\tВнимание!\nВедуться технические работы\nПриносим извинения за временные неудобства\n\n";
             system("pause");
             system("cls");
             break;
