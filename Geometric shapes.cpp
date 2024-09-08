b4320af9593012c833c892a95cf0a3e0e900011a
basalaeff <163347696+basalaeff@users.noreply.github.com>
2024-09-09 01:11:30 +0700

    Add menu

---
+++ Geometric shapes.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    while (true) {
+        cout << "\tMENU\t\n\n";
+        cout << "Геометрические фигуры" << endl;
+        cout << "1 - окружность" << endl;
+        cout << "2 - треугольник" << endl;
+        cout << endl << "Введите номер фигуры: ";
+        int shape = 0;
+        cin >> shape;
+        system("cls");
+        switch (shape) {
+        case 1:
+            cout << "Окружность" << endl << endl;
+            cout << "\tВнимание!\nВедуться технические работы\nПриносим извинения за временные неудобства\n\n";
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            cout << "Треугольник" << endl << endl;
+            cout << "\tВнимание!\nВедуться технические работы\nПриносим извинения за временные неудобства\n\n";
+            system("pause");
+            system("cls");
+            break;
+        default:
+            cout << " Фигура не выбрана! " << "\n";
+            Sleep(1000);
+            system("cls");
+            break;
+        }
+    }
+    return 0;
+}
+
